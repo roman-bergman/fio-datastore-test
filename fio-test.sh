8423374e7f1ce6c9277f50d2c90f18ec0a5c6638
Roman Bergman <89276354+roman-bergman@users.noreply.github.com>
2023-10-06 15:33:31 +0000

    Update fio-test.sh

--- fio-test.sh
+++ fio-test.sh
@@ -7,6 +7,7 @@
 
 #DEFINE
 disk=$1
+timestamp=$(date +%s)
 
 if [ -z "$disk" ] 
 then
@@ -22,7 +23,7 @@ fio_run () {
         #set -x
 
         echo "RUN TEST: $test_name"
-        fio --name=$test_name --filename=$disk --output-format=json --ioengine=libaio --direct=1 --randrepeat=0 "$@" > $test_name.json
+        fio --name=$test_name --filename=$disk --output-format=json --ioengine=libaio --direct=1 --randrepeat=0 "$@" > $timestamp-$test_name.json
         sleep 10
     )  
 }
@@ -38,14 +39,14 @@ fio_run read            --rw=read       --runtime=60    --bs=4M --numjobs=1     
 
 
 data="$(cat *.json | jq -sc . | gzip -9 | base64 -w0)"
-report_file=fio_result$(echo $disk | sed "s/\//\-/g").json
+report_file=$timestamp-fio_result$(echo $disk | sed "s/\//\-/g").json
 
 {
-for i in $(find ./ -name "*write*" | sort -V); do
+for i in $(find ./ -name "$timestamp*write*" | sort -V); do
         cat $i | jq -rc '.jobs[0] | {"name": ."job options".name, "iodepth": ."job options".iodepth, bs: ."job options".bs, jobs: ."job options".numjobs, "clat_ms": (.write.clat_ns.mean/1000)|floor, "bw_mbs": (.write.bw_mean/1024)|floor, iops: .write.iops_mean|floor}'
 done
 
-for i in $(find ./ -name "*read*" | sort -V); do
+for i in $(find ./ -name "$timestamp*read*" | sort -V); do
   cat $i | jq -rc '.jobs[0] | {"name": ."job options".name, "iodepth": ."job options".iodepth, bs: ."job options".bs, jobs: ."job options".numjobs, "clat_ms": (.read.clat_ns.mean/1000)|floor, "bw_mbs": (.read.bw_mean/1024)|floor, iops: .read.iops_mean|floor}'
 done
 } | jq -s "{\"$disk\": {results: ., data: \"$data\" }}" > $report_file
